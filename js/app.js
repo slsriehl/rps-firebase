5fcc6ca7659c2cc11ce83e760bfd702812ab15ac
Sarah Riehl <sls.riehl@gmail.com>
2016-09-05 22:09:43 -0500

    images, firebase, fonts

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCoP2rutXDNtMkqSleshn31PiY-NxjSbrk",
+    authDomain: "rps-7-9cc1d.firebaseapp.com",
+    databaseURL: "https://rps-7-9cc1d.firebaseio.com",
+    storageBucket: "rps-7-9cc1d.appspot.com",
+  };
+  firebase.initializeApp(config);
